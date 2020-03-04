576b41daab733f386d3af7485427919d310b2250
David Rearte <dengue8830@gmail.com>
2020-03-04 00:16:45 -0300

    clean

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,8 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import img1 from '../img/img1.jpg';
 import img2 from '../img/img2.jpg';
 import img3 from '../img/img3.jpg';
 import img4 from '../img/img4.jpg';
-// import logo from './logo.svg';
 import './App.css';
 import { Slider, Slide, Dot, Dots } from './slider';
 
